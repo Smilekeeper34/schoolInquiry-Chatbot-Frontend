16898bb449206e7379f6eb1d60338ae7e500753a
Garikai34 <garikai@clicknpay.africa>
2023-11-23 20:13:02 +0200

    changes

--- School-inquiry-chatbot/src/app/pages/chat/chat.component.ts
+++ School-inquiry-chatbot/src/app/pages/chat/chat.component.ts
@@ -13,12 +13,22 @@ export class ChatComponent implements OnInit {
     botResponse: new FormControl(''),
   });
 
+  currentDate: Date;
   chatMessages: any[] = [];
+  loading: boolean = false; // Add loading state
 
   constructor(private fb: FormBuilder, private chatService: ChatserviceService) {}
 
   ngOnInit() {
-    
+    this.updateCurrentDate();
+    setInterval(() => {
+      this.updateCurrentDate();
+    }, 60000);
+  }
+
+  updateCurrentDate() {
+    // Update the current date whenever needed
+    this.currentDate = new Date();
   }
 
   sendMessage() {
@@ -28,19 +38,22 @@ export class ChatComponent implements OnInit {
     // Convert userMessage to lowercase
     userMessage = userMessage.toLowerCase();
   
-    this.chatMessages.push({ content: userMessage, senderAvatar: '...user-avatar-url...' });
+    this.chatMessages.push({ content: userMessage, senderAvatar: 'https://uifaces.co/api-key-demo', type: 'user' });
+    this.loading = true; // Set loading to true
   
     this.chatService.sendMessage(userMessage).subscribe(
       response => {
         const botResponse = response.message;
-        this.chatMessages.push({ content: botResponse, senderAvatar: '...bot-avatar-url...' });
+        this.chatMessages.push({ content: botResponse, senderAvatar: 'https://uifaces.co/api-key-demo', type: 'bot' });
+
         this.messageForm.get('userMessage')?.setValue('');
-  
+        this.loading = false; 
         console.log(response);
       },
       error => {
         console.error('Error sending message:', error);
         // Handle the error, e.g., display an error message to the user
+        this.loading = false; 
       }
     );
   }
